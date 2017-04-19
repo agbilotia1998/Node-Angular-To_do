c16263c186b9eb42b914e1538345f216f1a78c05
agbilotia1998 <agbilotia1998@gmail.com>
2017-04-20 03:42:54 +0530

    Setted up authentication and various routes/API's - testing needed yet

--- app.js
+++ app.js
@@ -9,24 +9,179 @@ var express = require('express'),
     passportLocalMongoose = require("passport-local-mongoose"),
     cookieParser = require("cookie-parser"),
     mongoose = require('mongoose'),
-    database = require('./config/mongo_db'),
+    Schema = mongoose.Schema,
+    database = require("./config/mongo_db"),
+    User = require("./models/user.js"),
+    path = require('path'),
     app = express();
+    auth = require('passport-local-authenticate');
 
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.Promise = global.Promise;
+mongoose.connect(database.localUrl);
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(require("express-session")({
+    secret: "A to-do app",
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    },
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serialiseUser());
-passport.deserializeUser(User.deserialiseUser());
-mongoose.Promise = global.Promise;
-mongoose.connect(database.localUrl);
+    User.authenticate()));
+
+passport.serializeUser(function (User, err) {
+    User.serialiseUser()
+});
+
+passport.deserializeUser(function (User, err) {
+    User.deserialiseUser()
+});
 
 
 app.get('/', function (req, res) {
     res.render('index.ejs');
 });
+
+
+app.post("/registered", function (req, res) {
+    var details = {
+        username: req.body.username,
+        email: req.body.email,
+        confirmation: "0"
+    };
+    User.register(new User(details),
+        req.body.password, function (err, user) {
+            if (err) {
+                console.log(err);
+                return res.render('create.ejs', {b: "1"});
+            }
+            User.findOne({email: details.email}, function (err, found) {
+                if (!found) {
+                    passport.authenticate("local")(req, res, function () {
+                        res.render("registered.ejs", {username: details.username});
+                    });
+                }
+                else {
+                    console.log(err);
+                    return res.render('create.ejs', {b: "0"});
+                }
+            });
+        });
+});
+
+
+app.get("/confirmation/username/:un", function (req, res) {
+    var verify = (req.params.un);
+    User.findOne({username: (req.params.un)}, function (err, nameUser) {
+        if (nameUser) {
+            User.update({username: verify}, {$set: {confirmation: "1"}}, function (err, numUpdated) {
+                if (err) {
+                    console.log(err);
+                } else if (numUpdated) {
+                    console.log('Confirmation set to 1');
+                } else {
+                    console.log('No document found with defined "find" criteria!');
+                }
+            });
+            res.redirect("/login");
+        }
+        else {
+            res.send("USER NOT REGISTERED");
+        }
+    });
+});
+
+app.post("/login", function (req, res) {
+    var user = req.body.username;
+    var pass = req.body.password;
+    User.findOne({username: user, confirmation: "1"}, function (err, approved) {
+        if (!approved) {
+            res.redirect("/");
+            // alert("Invalid username or password");
+        }
+        else {
+            auth.hash(pass, function (err, hashed) {
+                console.log(hashed);
+                auth.verify(pass, hashed, function (err, verified) {
+                    if (verified) {
+                        req.session.username = user;
+                        res.send('entered');
+                    }
+
+                    else if (err)
+                        res.redirect('/');
+                });
+            });
+        }
+    })
+});
+
+
+app.get('/:user/todos', function (req, res) {
+
+    var user = req.params.user;
+    User.find({username: user}, function (err, todos) {
+        if (err)
+            res.send(err);
+
+        else
+            res.json(todos.task);
+    });
+});
+
+
+app.post('/:user/todos', function (req, res) {
+    var user = req.params.user;
+    var text = req.params.text;
+    User.findOne({username: user}, function (err, found) {
+        User.update({username: user}, {
+            $push: {Task: text},
+            $set: {Accomplished: false}
+        }, function (err, updated) {
+            if (err)
+                console.log(err);
+            else {
+                User.find({username: user}, function (err, todos) {
+                    if (err)
+                        res.send(err);
+
+                    else
+                        res.json(todos.task);
+                });
+            }
+        })
+    });
+});
+
+
+app.delete('/:user/delete/:todo_id', function (req, res) {
+    var user = req.params.user;
+    User.update({username: user}, {$unset: {task: ""}}, function (err, removed) {
+            if (err)
+                console.log(err);
+        }
+        , function (err, todo) {
+            if (err)
+                res.send(err);
+        });
+    User.find({username: user}, function (err, todos) {
+        if (err)
+            res.send(err);
+
+        else
+            res.json(todos.task);
+    });
+});
+
 
 app.listen(process.env.PORT || 5000, function () {
     console.log("SERVER STARTED");
---
+++ models/user.js
@@ -0,0 +1,16 @@
+/**
+ * Created by AYUSH on 4/20/2017.
+ */
+
+var mongoose = require('mongoose');
+var passportLocalMongoose=require('passport-local-mongoose');
+
+var UserSchema=new mongoose.Schema({
+    username:String,
+    password:String,
+    confirmation:String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+mongoose.model("User",UserSchema);
+module.exports=mongoose.model("user",UserSchema);
